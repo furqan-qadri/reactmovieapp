55dfb0b810fe727d37f97a0da2c95f952f3a51eb
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2024-02-14 17:39:25 +0800

    add header image and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from './search.svg';
 import { useEffect, useState } from "react";
 const API_URL='http://www.omdbapi.com?apikey=9dfb0d6a';
-
-const movie1={
-  "Title": "The Avengers",
-  "Year": "1998",
-  "imdbID": "tt0118661",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYWE1NTdjOWQtYTQ2Ny00Nzc5LWExYzMtNmRlOThmOTE2N2I4XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
-}
-
+import Footer from "./Footer";
 
 function App() {
   const [movies, setMovies]= useState([]);
@@ -31,34 +23,39 @@ function App() {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
-      <div className="search">
-      <input
-        placeholder="Search for movies"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            searchMovies(searchTerm);
-          }
-        }}
-      />
-      <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
-      </div>
+        <div className="top-div">
+        <img src="/movies.jpeg" alt="movies logo" class="header-image" />
+        <h1>What's That Movie?</h1>  
+        </div>
+        
+        <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchMovies(searchTerm);
+            }
+          }}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+        </div>
 
-      if {
-        movies?.length > 0 ? (
-          <div className="container">
-            {movies.map((movie)=> (
-              <MovieCard movie={movie} />
-            ))}
-          </div>
-        ) :  (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )
-      }
+        {
+          movies?.length > 0 ? (
+            <div className="container">
+              {movies.map((movie)=> (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) :  (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+        }
+        <Footer/>
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'> <a href="">Made with ❤️ by Furqan Qadri</a></div>
+  )
+}
+
+export default Footer
